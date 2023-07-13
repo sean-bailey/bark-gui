b5f78faefc3c3dae1bf4214f039806f0dc4bd303
sean-bailey <seanbailey518@gmail.com>
2023-07-13 16:48:21 -0400

    checking forced settings update

--- bark/settings.py
+++ bark/settings.py
@@ -1,7 +1,7 @@
 import os
 
 def initenv(args):
-    os.environ['SUNO_USE_SMALL_MODELS'] = str("-smallmodels" in args)
-    os.environ['BARK_FORCE_CPU'] = str("-forcecpu" in args)
-    os.environ['SUNO_ENABLE_MPS'] = str("-enablemps" in args)
-    os.environ['SUNO_OFFLOAD_CPU'] = str("-offloadcpu" in args)
+    os.environ['SUNO_USE_SMALL_MODELS'] = True#str("-smallmodels" in args)
+    os.environ['BARK_FORCE_CPU'] = True#str("-forcecpu" in args)
+    os.environ['SUNO_ENABLE_MPS'] = False#str("-enablemps" in args)
+    os.environ['SUNO_OFFLOAD_CPU'] = True#str("-offloadcpu" in args)
