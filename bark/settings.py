5d1bab28ecd586a6c45036a7c924fe87612f99f0
sean-bailey <seanbailey518@gmail.com>
2023-08-04 23:25:17 -0400

    testing build

--- bark/settings.py
+++ bark/settings.py
@@ -1,7 +1,7 @@
 import os
 
 def initenv(args):
-    os.environ['SUNO_USE_SMALL_MODELS'] = True#str("-smallmodels" in args)
-    os.environ['BARK_FORCE_CPU'] = True#str("-forcecpu" in args)
-    os.environ['SUNO_ENABLE_MPS'] = False#str("-enablemps" in args)
-    os.environ['SUNO_OFFLOAD_CPU'] = True#str("-offloadcpu" in args)
+    os.environ['SUNO_USE_SMALL_MODELS'] = "True"#str("-smallmodels" in args)
+    os.environ['BARK_FORCE_CPU'] = "True"#str("-forcecpu" in args)
+    os.environ['SUNO_ENABLE_MPS'] = "False"#str("-enablemps" in args)
+    os.environ['SUNO_OFFLOAD_CPU'] = "True"#str("-offloadcpu" in args)
